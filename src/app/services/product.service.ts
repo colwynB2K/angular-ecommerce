83007aa3bf80f7961b97759d1a58f1cc2941189e
eersels3764 <ivo.eersels@idewe.be>
2021-09-09 11:04:24 +0200

    Create Angular service to call REST API
    			Angular service = helper class developed in TypeScript which runs in the web browser (client-side) as part of your Angular app
    
    			* Angular provides a REST client called HttpClient which is part of HttpClientModule
    			* We need to add support for it in the application module (src/app/app.module.ts)
    				@NgModule entry needs to mention HttpClientModule in the section imports: []
    
    				And in the providers section we need to mention our ProductService so it can be injected into other components/classes.
    
    					import { HttpClientModule } from "@angular/common/http";
    
    					import { AppComponent } from './app.component';
    					import { ProductListComponent } from './components/product-list/product-list.component';
    					import { ProductService } from "./services/product.service";
    
    					@NgModule({
    					  declarations: [
    						AppComponent,
    						ProductListComponent
    					  ],
    					  imports: [
    						BrowserModule,
    						HttpClientModule
    					  ],
    					  providers: [ProductService],
    					  bootstrap: [AppComponent]
    					})
    					export class AppModule { }
    
    			'ng.cmd generate service services/product'
    
    			Mind the proper imports in the service code!!!

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Product } from "../common/product";
+import { map } from "rxjs/operators";
+
+// Angular has built in support for a dependency injection framework
+// This service can be injected into other components or class as a dependency
+@Injectable({
+  providedIn: 'root'				// You can inject it anywhere within the Angular App, not only in certain components or classes
+})
+export class ProductService {
+
+  private baseUrl = 'http://localhost:8080/api/products';
+
+  // Our service also uses dependency injection... the HttpClient is injected into it via constructor injection
+  constructor(private httpClient: HttpClient) { }
+
+  // This function returns an Observable so components/classes can subscribe to it
+  getProductList(): Observable<Product[]> {
+    return this.httpClient.get<GetResponse>(this.baseUrl)
+                          .pipe(
+                            map(response => response._embedded.products)
+                          ); // This describes the structure of the JSON response from Spring Data REST and map it to a Product array voa the GetResponse interface
+  }
+}
+
+interface GetResponse {
+  _embedded: {
+    products: Product[];			// Unwrap the JSON from the _embedded JSON entry and map it to a Product array
+  }
+}
