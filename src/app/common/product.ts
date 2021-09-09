accfebe966fd604b80b712a8d3343b4fc6fce3ce
eersels3764 <ivo.eersels@idewe.be>
2021-09-09 10:42:58 +0200

    Develop TypeScript class for Product 			'ng.cmd generate class common/product'							(NOTE: common subfolder)
    			export class Product {
    				// Define same properties as exposed by the REST API
    
    			}

---
+++ src/app/common/product.ts
@@ -0,0 +1,11 @@
+export class Product {
+  active: boolean;
+  created: Date;
+  description: string;
+  imageUrl: string;
+  lastUpdated: Date;
+  name: string;
+  sku: string;
+  unitPrice: number;
+  unitsInStock: number;
+}
