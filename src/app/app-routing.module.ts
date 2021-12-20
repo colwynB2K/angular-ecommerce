5521be64b50c9ee0486f114c38925ef35986c5e7
eersels3764 <ivo.eersels@idewe.be>
2021-12-20 19:50:36 +0100

    Angular Project - Search for Products by Category _________________________________________________
    Angular Project - Search for Products by Category - Overview
    ------------------------------------------------------------
    
    	3) Define the Router Outlet
    
        			* Router Outlet acts as a placeholder (the part of the page where the view will be updated)
        			* Renders the desired component based on route
    
        			* Update app.component.html to use Router Outlet
    
        				<!-- MAIN CONTENT -->
        				<router-outlet></router-outlet>							<!-- Display component based on router configuration -->
    
        				<!-- <app-product-list></app-product-list> -->
    
        		4) Set up Router Links to pass category id param
    
        			* In our HTML set up links to our route which contain a category id as a parameter
    
        				...
        				<!-- MENU SIDEBAR -->
        				<li>
        					<a routerLink="/category/1" routerLinkActive="active-link">Books</a>					<!-- routerLink defines the link path and routerLinkActive defines the CSS class to apply when the link is active (=optional) -->
        				</li>
        				<li>
        					<a routerLink="/category/2" routerLinkActive="active-link">Coffee Mugs</a>
        				</li>
        				...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import {ProductListComponent} from "./components/product-list/product-list.component";
 
 const routes: Routes = [
-  {path: 'product-categories/:id', component: ProductListComponent},					// PATH HAS NO LEADING SLASHES!!!
-  {path: 'product-categories', component: ProductListComponent},						// PATH HAS NO LEADING SLASHES!!!, no id param specified default id is used inside the component
+  {path: 'category/:id', component: ProductListComponent},					// PATH HAS NO LEADING SLASHES!!!
+  {path: 'categories', component: ProductListComponent},						// PATH HAS NO LEADING SLASHES!!!, no id param specified default id is used inside the component
   {path: 'products', component: ProductListComponent},								// PATH HAS NO LEADING SLASHES!!!, no id param specified default id is used inside the component
   {path: '', redirectTo: '/products', pathMatch: 'full'},								// REDIRECT PATH HAS LEADING SLASHES!!!, full means to match on this path exactly (default value is 'prefix', matches if path starts with this value)
   {path: '**', redirectTo: '/products', pathMatch: 'full'}							// REDIRECT PATH HAS LEADING SLASHES!!!, default route if none of the above match
