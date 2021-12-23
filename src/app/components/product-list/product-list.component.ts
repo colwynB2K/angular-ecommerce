699d3a3cb8a635606f9173db2c4ac4d769fd0213
eersels3764 <ivo.eersels@idewe.be>
2021-12-23 10:40:16 +0100

    eCommerce Project - Search for Products by Keyword ____________________________________________________
    Angular Project - Search for Products by Keyword - Overview
    -----------------------------------------------------------
    
    DEVELOPMENT PROCESSS
    
    	5) Enhance ProductListComponent to search for products with ProductService
    
        		product-list.component.ts
    
        		const keyword: string = this.route.snapshot.paramMap.get('keyword');
    
        		// now search for the products using keyword
        		this.productService.searchProductsByKeyword(keyword).subscribe(
        			data => {
        				this.products = data;
        			}
        		)

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -31,20 +31,32 @@ export class ProductListComponent implements OnInit {
     // check if 'id' parameter is available, so we know we can use it or need to use a default
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');	// route.snapshot means use the activated route and get its current state. From that get the Map of all the route parameters. Check that it has a 'id'  parameter value.
 
-    if (hasCategoryId) {
-      // Get the 'id' value and convert the string to a number using the '+' symbol
-      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;  // ! Do no concern about possible null values
-    } else {
-      // no category id available, just default to category id 1
-      this.currentCategoryId = 1;
-    }
+    // check if 'keyword' parameter is available. so we know a search by keyword was performed
+    const hasKeyword: boolean = this.route.snapshot.paramMap.has('keyword');
 
-    // Get the products for the given category id
-    this.productService.getProductList(this.currentCategoryId).subscribe(			// Get product list via asynchronous REST call and subscribe to the response to process it when it arrives
-      data => {
-        this.products = data;							              // Assign the Product[] coming from the ProductService to the products property in this ProductListComponent class
+    if (hasKeyword) {
+      // now search for the products using keyword
+      this.productService.searchProductsByKeyword(this.route.snapshot.paramMap.get('keyword')).subscribe(
+        data => {
+          this.products = data;
+        }
+      )
+    } else {
+      if (hasCategoryId) {
+        // Get the 'id' value and convert the string to a number using the '+' symbol
+        this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;  // ! Do no concern about possible null values
+      } else {
+        // no category id available, just default to category id 1
+        this.currentCategoryId = 1;
       }
-    )
+
+      // Get the products for the given category id
+      this.productService.getProductList(this.currentCategoryId).subscribe(			// Get product list via asynchronous REST call and subscribe to the response to process it when it arrives
+        data => {
+          this.products = data;							              // Assign the Product[] coming from the ProductService to the products property in this ProductListComponent class
+        }
+      )
+    }
   }
 
 }
