5bb22ebf102713bb89f873639ed047fcc87e7b95
eersels3764 <ivo.eersels@idewe.be>
2022-01-10 17:21:07 +0100

    eCommerce Project - Product Master-Detail View (PDP) - Overview _______________________________________________________________
    Angular Project - Product Master-Detail View (PDP) - Overview
    -------------------------------------------------------------
    
    	* Development Process
    
    		2) Add new Angular route for PDP
    
            			In app-routing.module.ts
    
            			...
            			{path: 'products/:id', component: ProductDetailsComponent},
            			...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {ProductListComponent} from "./components/product-list/product-list.component";
+import { ProductListComponent } from "./components/product-list/product-list.component";
+import { ProductDetailsComponent } from "./components/product-details/product-details.component";
 
 const routes: Routes = [
   {path: 'search/:keyword', component: ProductListComponent},
   {path: 'category/:id', component: ProductListComponent},					// PATH HAS NO LEADING SLASHES!!!
   {path: 'categories', component: ProductListComponent},						// PATH HAS NO LEADING SLASHES!!!, no id param specified default id is used inside the component
   {path: 'products', component: ProductListComponent},								// PATH HAS NO LEADING SLASHES!!!, no id param specified default id is used inside the component
+  {path: 'products/:id', component: ProductDetailsComponent},         // PDP
   {path: '', redirectTo: '/products', pathMatch: 'full'},								// REDIRECT PATH HAS LEADING SLASHES!!!, full means to match on this path exactly (default value is 'prefix', matches if path starts with this value)
   {path: '**', redirectTo: '/products', pathMatch: 'full'}							// REDIRECT PATH HAS LEADING SLASHES!!!, default route if none of the above match
 ];
