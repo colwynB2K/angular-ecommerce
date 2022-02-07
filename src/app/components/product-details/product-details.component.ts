5ecd766a7969231ca45cba28679537d8144ecd26
eersels3764 <ivo.eersels@idewe.be>
2022-02-07 15:25:08 +0100

    eCommerce Project - Product Master-Detail View (PDP) - Overview _______________________________________________________________
    Angular Project - Product Master-Detail View (PDP) - Overview
    -------------------------------------------------------------
    
    	* Development Process
    
    				4) Enhance ProductDetailsComponent to retrieve product from ProductService
    
            			...
            				// Get the 'id' param string, convert it to a number using the '+'symbol
            				const productId: number = +this.route.snapshot.paramMap.get('id');
    
            				this.productService.getProduct(productId).subscribe(
            					data => {
            						this.product = data;
            					}
            				)
            			...

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from "../../services/product.service";
+import { ActivatedRoute } from "@angular/router";
+import {Product} from "../../common/product";
 
 @Component({
   selector: 'app-product-details',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  product: Product | undefined;
 
-  ngOnInit(): void {
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) {
   }
 
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(() => {
+      this.handleProductDetails();
+    })
+  }
+
+  private handleProductDetails() {
+    const productIdString: string | null = this.route.snapshot.paramMap.get('id');
+    if (productIdString) {
+      // Get the 'id' param string, convert it to a number using the '+'symbol
+      const productId: number = +productIdString;
+
+      this.productService.getProduct(productId).subscribe(
+        data => {
+          this.product = data;
+        }
+      )
+
+    }
+  }
 }
